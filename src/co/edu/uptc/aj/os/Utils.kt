2dd5dc71874d66d52c3f9df360b51735a5564cd7
jahirfiquitiva <jahir.fiquitiva@gmail.com>
2018-06-29 00:00:39 -0500

    Final fixes

--- src/co/edu/uptc/aj/os/Utils.kt
+++ src/co/edu/uptc/aj/os/Utils.kt
@@ -1,7 +1,5 @@
 package co.edu.uptc.aj.os
 
-import java.math.RoundingMode
-import java.text.DecimalFormat
 import java.util.Random
 
 fun Random.intBetween(min: Int, max: Int): Int = nextInt(max + 1 - min) + min
