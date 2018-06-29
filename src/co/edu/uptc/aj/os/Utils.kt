2f628f840525c79a1c07b31696d12e03de195977
jahirfiquitiva <jahir.fiquitiva@gmail.com>
2018-06-28 19:07:45 -0500

    Remove unnecessary method

--- src/co/edu/uptc/aj/os/Utils.kt
+++ src/co/edu/uptc/aj/os/Utils.kt
@@ -6,14 +6,6 @@ import java.util.Random
 
 fun Random.intBetween(min: Int, max: Int): Int = nextInt(max + 1 - min) + min
 
-fun Number.round(decimalCount: Int): String {
-    val expression = StringBuilder().append("#.")
-    (1..decimalCount).forEach { expression.append("#") }
-    val formatter = DecimalFormat(expression.toString())
-    formatter.roundingMode = RoundingMode.HALF_UP
-    return formatter.format(this)
-}
-
 fun ignored(what: () -> Unit) {
     try {
         what()
